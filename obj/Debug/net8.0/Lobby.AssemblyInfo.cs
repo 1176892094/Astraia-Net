33b7cdb291c17bdd18808ff5c4c85b3efa14fe67
Charlotte <75679851+1176892094@users.noreply.github.com>
2026-05-26 21:22:36 +0800

    Update Lobby.AssemblyInfo.cs

--- obj/Debug/net8.0/Lobby.AssemblyInfo.cs
+++ obj/Debug/net8.0/Lobby.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Astraia.Lobby")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c394ef7adfde9903a359e3b066b021d2df01a4ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb3ebc79262a25dde045fe0ba1f45e55255da2b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Astraia.Lobby")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Astraia.Lobby")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
